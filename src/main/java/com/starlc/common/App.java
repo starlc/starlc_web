11e90e63e2226bc4349583fa1c91d1c6a73a91fe
starlc <starlc_2588@163.com>
2017-11-30 15:18:13 +0800

    add

---
+++ src/main/java/com/starlc/common/App.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+ * Copyright (C) 2014 xx
+ *****************************************************************************/
+
+package com.starlc.common;
+
+/**
+ * xx
+ *
+ * @author xx
+ * @since jdk1.6
+ * @version 2017年11月30日 xx
+ */
+public class App {
+    
+    /**
+     * xx
+     * 
+     * @return xx
+     */
+    public String getGreeting() {
+        return "Hello world.";
+    }
+    
+    /**
+     * xx
+     * 
+     * @param args xx
+     */
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
---
+++ src/main/java/com/starlc/springboot/Application.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * Copyright (C) 2014 xx
+ *****************************************************************************/
+
+package com.starlc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * xx
+ *
+ * @author xx
+ * @since jdk1.6
+ * @version 2017年11月30日 xx
+ */
+@SpringBootApplication
+public class Application {
+    
+    /**
+     * xx
+     * 
+     * @param args xx
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
