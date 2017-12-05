6a2c52ef8e0b94a9e80a148a52b794cdf2df1d24
starlc <starlc_2588@163.com>
2017-12-05 23:03:11 +0800

    添加测试类

---
+++ src/main/java/com/starlc/common/App.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+* Copyright (C) 2017 Starlc
+*****************************************************************************/
+ 
+package com.starlc.common; 
+
+/**
+ * 类注释信息
+ * @Description:   测试类
+ * 
+ * @author   starlc
+ * @since	 jdk1.7
+ * @version  V-0.1
+ * @Date     2017年12月5日 starlc      
+ */
+public class App {
+    
+    public String getGreeting() {
+        return "Hello world.";
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+    
+    public String test(){
+        return "";
+    }
+
+}
+ 
