70f81e57754389eb54a9141a9ebb158a802f43c6
starlc <starlc_2588@163.com>
2017-12-18 10:15:35 +0800

    修改

--- src/main/java/com/starlc/mybatis/extend/plugin/LogInterceptor.java
+++ src/main/java/com/starlc/mybatis/extend/plugin/LogInterceptor.java
@@ -1,8 +1,8 @@
 /******************************************************************************
-* Copyright (C) 2017 Starlc
-*****************************************************************************/
- 
-package com.starlc.mybatis.extend.plugin; 
+ * Copyright (C) 2017 Starlc
+ *****************************************************************************/
+
+package com.starlc.mybatis.extend.plugin;
 
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.logging.Log;
@@ -13,28 +13,30 @@ import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
 
 /**
  * 类注释信息
- * @Description:   打印SQL插件
  * 
- * @author   starlc
- * @since	 jdk1.7
- * @version  V-0.1
- * @Date     2017年12月5日 starlc      
+ * @Description: 打印SQL插件
+ * 
+ * @author starlc
+ * @since jdk1.7
+ * @version V-0.1
+ * @Date 2017年12月5日 starlc
  */
 public class LogInterceptor {
+    
+    /** 日志 */
     private static final Log logger = LogFactory.getLog(LogInterceptor.class);
-
+    
     /**
      * 打印SQL
      * 
      * @param statementHandler
-     * @throws Throwable
      */
-    public void intercept(StatementHandler statementHandler) throws Throwable {
-        MetaObject metaStatementHandler = MetaObject.forObject(statementHandler,new DefaultObjectFactory(),new DefaultObjectWrapperFactory());
+    public void intercept(StatementHandler statementHandler) {
+        MetaObject metaStatementHandler = MetaObject.forObject(statementHandler, new DefaultObjectFactory(),
+            new DefaultObjectWrapperFactory());
         String originalSql = (String) metaStatementHandler.getValue("delegate.boundSql.sql");
         Object parame = metaStatementHandler.getValue("delegate.boundSql.parameterObject");
         logger.debug("SQL:" + originalSql);
         logger.debug("parame:" + parame);
     }
 }
- 
