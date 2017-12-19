6410ce76a9ade74e76340920c2e58b8d28217882
starlc <starlc_2588@163.com>
2017-12-19 11:43:02 +0800

    修改

--- src/main/java/com/starlc/base/orm/dao/CoreDAO.java
+++ src/main/java/com/starlc/base/orm/dao/CoreDAO.java
@@ -1,19 +1,27 @@
 /******************************************************************************
-* Copyright (C) 2017 Starlc
-*****************************************************************************/
- 
-package com.starlc.base.orm.dao; 
+ * Copyright (C) 2017 Starlc
+ *****************************************************************************/
+
+package com.starlc.base.orm.dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.starlc.base.orm.model.CoreVO;
 
 /**
  * 类注释信息
- * @Description:   TODO(用一句话描述该文件做什么) 
  * 
- * @author   starlc
- * @since	 jdk1.7
- * @version  V-0.1
- * @Date     2017年12月5日 starlc      
+ * @Description: TODO(用一句话描述该文件做什么)
+ * 
+ * @author starlc
+ * @since jdk1.7
+ * @version V-0.1
+ * @param <T> 类型
+ * @Date 2017年12月5日 starlc
  */
-public class CoreDAO<T> {
-
+public class CoreDAO<T extends CoreVO> {
+    
+    //
+    private static Logger logger = LoggerFactory.getLogger(CoreDAO.class);
 }
- 
